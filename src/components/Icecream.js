7e4c49eade1fc8792354689507828ed6ad96f6c0
kozakevich13 <63108530+kozakevich13@users.noreply.github.com>
2022-06-30 18:45:45 +0300

    first commit

---
+++ src/components/Icecream.js
@@ -0,0 +1,13 @@
+import icecream from '../img/icecream.png';
+
+import './Icecream.css';
+
+function Icecream() {
+  return (
+    <div className="App">
+        <img src={icecream} className="App-logo" alt="logo" />
+    </div>
+  );
+}
+
+export default Icecream;
