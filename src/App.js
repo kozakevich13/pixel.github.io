ad7710a90b59c85e9369583a7a6d7e70bb14ca29
kozakevich13 <63108530+kozakevich13@users.noreply.github.com>
2022-07-24 13:52:37 +0300

    add shapes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Icecream from '../src/components/Icecream'
+import Shapes from './components/shapes/shapes';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
         hello world
-        <Icecream></Icecream>
+        <Icecream/>
+        <Shapes/>
     </div>
   );
 }
---
+++ src/components/shapes/shapes.js
@@ -0,0 +1,9 @@
+import './shapes.css';
+
+function Shapes() {
+  return (
+    <div className="shapes-6"></div>
+  );
+}
+
+export default Shapes;
