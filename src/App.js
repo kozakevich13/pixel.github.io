7afdaa2534c1999f35bc529c21d3d374e2a73340
kozakevich13 <63108530+kozakevich13@users.noreply.github.com>
2022-07-27 20:06:23 +0300

    add bear

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import Icecream from '../src/components/Icecream'
 import Shapes from './components/shapes/shapes';
 import './App.css';
 import Loader from './components/loader/loader';
+import Bear from './components/spline/bear/bear';
 
 function App() {
   return (
     <div className="App">
-        <Icecream/>
-        <Shapes/>
-        <Loader/>
+        {/* <Icecream/> */}
+        {/* <Shapes/> */}
+        {/* <Loader/> */}
+        <Bear/>
     </div>
   );
 }
---
+++ src/components/spline/bear/bear.js
@@ -0,0 +1,7 @@
+import Spline from '@splinetool/react-spline';
+
+export default function Bear() {
+  return (
+    <Spline scene="https://prod.spline.design/uegpakNJgxDHAJv2/scene.splinecode" />
+  );
+}
